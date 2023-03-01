e062a592184a9e9b6c5e51e9ad3b27403765eaf1
afkfish <63806903+afkfish@users.noreply.github.com>
2023-03-01 12:48:09 +0100

    chore: nem kell view egeszen a vegsokig

--- src/main/java/com/example/application/Main.java
+++ src/main/java/com/example/application/Main.java
@@ -1,30 +1,22 @@
 package com.example.application;
 
-import com.formdev.flatlaf.FlatDarkLaf;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class Main {
+	private enum Version {
+		SKELETON, PROTOTYPE, GRAPGHICAL
+	}
+	public static Version version = Version.SKELETON;
 	public static void main(String[] args) {
 		Logger logger = LogManager.getLogger(Main.class);
 		logger.info("Hello World!");
-		try {
-			UIManager.setLookAndFeel( new FlatDarkLaf() );
-		} catch( Exception ex ) {
-			System.err.println( "Failed to initialize LaF" );
+
+
+		if (version == Version.GRAPGHICAL) {
+			JFrame mainFrame = View.setup();
 		}
-
-
-		JFrame frame = new JFrame("Hello World");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
-		int height = size.height;
-		int width = size.width;
-		frame.setSize(width / 2, height / 2);
-		frame.setLocationRelativeTo(null);
-		frame.setVisible(true);
 	}
 }
