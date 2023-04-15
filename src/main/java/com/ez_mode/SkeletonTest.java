1d965b3bc6c20dea09dfac0e143f79d8f371d62e
Bence Farkas <bencefarkas27@edu.bme.hu>
2023-04-15 11:29:39 +0200

    SkeletonTest class létrehozva

---
+++ src/main/java/com/ez_mode/SkeletonTest.java
@@ -0,0 +1,25 @@
+package com.ez_mode;
+import com.ez_mode.characters.Character;
+import com.ez_mode.objects.*;
+import com.ez_mode.characters.*;
+
+public class SkeletonTest {
+    public void SetPumpTest(){
+
+    }
+    public void PlumberMovesToPumpTest(){}
+    public void PlumberMovesToPipeTest(){}
+    public void NomadBreaksPipeTest(){
+        Nomad nomad = new Nomad("Nomad");
+        Pipe pipe = new Pipe();
+    }
+    public void NomadMovesToPipeTest(){}
+    public void CharacterMovesTest(){}
+    public void CharacterMovesToPumpTest(){}
+    public void PlumberRepairsPipeTest(){}
+    public void PlumberRepairsPumpTest(){}
+    public void PlumberPicksUpPipe(){}
+    public void PlumberPicksUpPump(){}
+    public void PlumberDeploysUpPump(){}
+    public void PlumberMovesToCistern(){}
+}
