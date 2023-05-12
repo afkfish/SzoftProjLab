1f6e319421cce49f869b4c8aceeae2603eb81aac
Bence Farkas <bencefarkas27@edu.bme.hu>
2023-05-12 11:51:22 +0200

    tests

--- src/main/java/com/ez_mode/ProtoTest.java
+++ src/main/java/com/ez_mode/ProtoTest.java
@@ -1,3 +1,22 @@
 package com.ez_mode;
 
-public class ProtoTest {}
+public class ProtoTest {
+    public ProtoTest(){
+
+    }
+    public void MapPrintTest(){
+
+    }
+    public void CharacterAddTest(){}
+    public void AddNewPumpTest(){}
+    public void AddNewPipeTest(){}
+    public void PlacePumpTest(){}
+    public void PlacePipeTest(){}
+    public void MoveCharacterTest(){}
+    public void BreakPipeTest(){}
+    public void SetPumpTest(){}
+    public void RepairPumpTest(){}
+    public void RepairPipeTest(){}
+    public void MakePipeSlipperyTest(){}
+    public void MakePipeStickyTest(){}
+}
