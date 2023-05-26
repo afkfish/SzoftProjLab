25412b4239a50fb529385422f28e7e06f6d025d3
Péter Nyilas <nyilaspeter001@gmail.com>
2023-05-26 15:29:31 +0200

    Pumpa bugfix #2

--- src/main/java/com/ez_mode/objects/Pump.java
+++ src/main/java/com/ez_mode/objects/Pump.java
@@ -1,11 +1,12 @@
 package com.ez_mode.objects;
 
-import static java.lang.Double.min;
-
 import com.ez_mode.characters.Character;
 import com.ez_mode.characters.Nomad;
 import com.ez_mode.exceptions.InvalidPlayerActionException;
+
 import java.util.Random;
+
+import static java.lang.Double.min;
 
 /**
  * A pump is a node that can be adjusted and repaired. It is bound to break after a certain amount
@@ -101,9 +102,12 @@ public class Pump extends Node {
         } else {
           internalBufferLevel += activeInput.flowRate;
         }
-      } else if (this.internalBufferLevel > 0) {
+      } else if (this.internalBufferLevel > 0 &&sources.contains(activeOutput)) {
         this.setFlowRate(min(this.internalBufferLevel, activeOutput.getCapacity()));
         activeOutput.flowRate += flowRate;
+      }
+      else{
+        return;
       }
     } else {
       this.setFlowRate(0);
